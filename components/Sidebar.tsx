bee52796ab6368c712efa4a595bc22b6fb8884b8
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-11-25 23:27:05 -0300

    creating sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { 
+  BellIcon, 
+  HashtagIcon, 
+  BookmarkIcon,   
+  UserIcon,
+  HomeIcon,
+} from '@heroicons/react/24/outline';
 
 const Sidebar = () => {
   return (
     <div>
-      Sidebar
+      <img className='h-10 w-10' src='https://links.papareact.com/drq'/>
     </div>
   )
 }
 
 export default Sidebar
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 import Head from 'next/head'
-import Image from 'next/image'
 import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
