9c4d29b59f5b3ce215f11b680f4a5355236e9846
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-11-26 11:38:11 -0300

    finishing sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,11 +6,17 @@ import {
   UserIcon,
   HomeIcon,
 } from '@heroicons/react/24/outline';
+import SidebarRow from './SidebarRow';
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='flex flex-col'>
       <img className='h-10 w-10' src='https://links.papareact.com/drq'/>
+      <SidebarRow Icon={HomeIcon} title="Home"/>
+      <SidebarRow Icon={HashtagIcon} title="Explore"/>
+      <SidebarRow Icon={BellIcon} title="Notifications"/>
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks"/>
+      <SidebarRow Icon={UserIcon} title="Sign In"/>
     </div>
   )
 }
